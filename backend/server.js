4b613516afbaf6e45e0ce575db4558c483584d20
Khang Tran <116851477+NPKLTacocat@users.noreply.github.com>
2025-06-24 19:03:54 +0700

    Setup basic routes, controllers and connection to MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,16 @@
 import express from "express";
+import authRoutes from "./routes/auth.routes.js";
+import dotenv from "dotenv";
+import connectMongoDB from "./db/connectMongoDB.js";
+
+dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-  res.send("Server is ready");
-});
+app.use("/api/auth", authRoutes);
 
-app.listen(8000, () => {
-  console.log("Server is running on port 8000");
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+  connectMongoDB();
 });
