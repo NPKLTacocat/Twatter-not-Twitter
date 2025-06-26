4dbee1778dbcb9f2a8cdfc083522b7d1a5924f8a
Khang Tran <116851477+NPKLTacocat@users.noreply.github.com>
2025-06-26 21:21:39 +0700

    Controllers for auth finished

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,5 +1,11 @@
 import express from "express";
-import { signup, login, logout } from "../controllers/auth.controller.js";
+import {
+  signup,
+  login,
+  logout,
+  getMe,
+} from "../controllers/auth.controller.js";
+import { protectedRoute } from "../middleware/protectedRoute.js";
 
 const router = express.Router();
 
@@ -8,5 +14,7 @@ router.post("/signup", signup);
 router.post("/login", login);
 
 router.post("/logout", logout);
+
+router.post("/me", protectedRoute, getMe);
 
 export default router;
